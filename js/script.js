e04fac51f330f274353b0c391c0a71c814cb358c
samuel-popoca <cybersampromax@gmail.com>
2026-03-16 15:41:10 -0600

    Samuel Company X

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const menuToggle = document.getElementById('mobile-menu');
+const navList = document.querySelector('.nav-list');
+
+menuToggle.addEventListener('click', () => {
+  navList.classList.toggle('active');
+});
+
